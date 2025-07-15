87292e4c452a920724fa458e5de30ea029de467d
Bihan Dasgupta <dasguptb@uci.edu>
2025-07-15 12:01:15 -0400

    changing rerouting

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,15 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   define: {
     'process.env': process.env
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        journal: 'journal.html',
+        draw: 'draw.html',
+        dreams: 'dreams.html'
+      }
+    }
   }
 })
